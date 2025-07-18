e461428c0d7ef026d59324e1174d427e0cbc4693
Guilherme Cardoso <cardoso.guivi@gmail.com>
2025-07-18 18:32:34 -0300

    add parcial tests.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as rows;");
-  console.log(result.rows);
-  response.status(200).json({ vasco: "campeão" });
+  const updatedAt = new Date().toISOString();
+  const { rows: resultVersion } = await database.query("SHOW SERVER_VERSION;");
+  const { rows: resultMaxConnections } = await database.query(
+    "SHOW MAX_CONNECTIONS;",
+  );
+  const { rows: resultActiveConnections } = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE datname='local_db' AND state='active';",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: parseInt(resultVersion[0].server_version),
+      max_connections: parseInt(resultMaxConnections[0].max_connections),
+      active_connections: parseInt(resultActiveConnections[0].count),
+    },
+  });
 }
 
 export default status;
